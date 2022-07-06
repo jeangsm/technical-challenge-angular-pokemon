a15649010c3baff8a91cea8d00e94a8039d0a2bd
Jean Medeiros <jeangsmedeiros@gmail.com>
2022-07-06 11:21:54 -0300

    :wrench: Toggle navbar fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { FavoritesModel, Pokemon } from 'src/app/models';
@@ -8,11 +9,27 @@ import { FavoritesModel, Pokemon } from 'src/app/models';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   favorites$: Observable<Pokemon[]>;
+  navbarOpen = false;
+
   constructor(
     private store: Store<FavoritesModel>,
+    private router: Router
   ) {
     this.favorites$ = this.store.pipe(select('favorites'));
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationStart) {
+        this.navbarOpen = false;
+      }
+    });
+  
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen
   }
 }
